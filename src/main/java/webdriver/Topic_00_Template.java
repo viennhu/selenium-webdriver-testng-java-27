43b1aae20835c33e98bc93c99623940a61f1e802
Vivian Le <vivian.le@avepoint.com>
2022-07-20 16:10:23 +0700

    add new Topics

---
+++ src/main/java/webdriver/Topic_00_Template.java
@@ -0,0 +1,65 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_00_Template {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("MAC OS")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        }
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_() {
+
+
+    }
+
+    @Test
+    public void TC_02_() {
+
+    }
+
+    @Test
+    public void TC_03_() {
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    public void sleepInSecond (long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int generateRandomNumber () {
+        Random rand = new Random();
+        return rand.nextInt(99999);
+    }
+}
