27ec06927edf328fef63f682854337d1c3fb74e5
Vivian Le <vivian.le@avepoint.com>
2022-07-21 15:53:26 +0700

    add new Topics

--- src/main/java/webdriver/Topic_16_UploadFile.java
+++ src/main/java/webdriver/Topic_16_UploadFile.java
@@ -1,12 +1,16 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -15,25 +19,58 @@ public class Topic_16_UploadFile {
     String projectPath = System.getProperty("user.dir");
     String osName = System.getProperty("os.name");
 
+    //Image name
+    String rabbit = "rabbit.jpg";
+    String hippo = "hippo.jfif";
+    String tiger = "tiger.jfif";
+
+    //Folder Path Name
+    String folderPathName = projectPath + File.separator + "picforuploading" + File.separator;
+
     @BeforeClass
     public void beforeClass() {
-        System.setProperty("webdriver.gecko.driver", projectPath + File.separator + "browserDrivers" + File.separator + "geckodriver");
+        System.setProperty("webdriver.gecko.driver", projectPath + File.separator + "browserDrivers" + File.separator + "geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
     @Test
-    public void TC_UploadFile_SendKeys() {
+    public void TC_UploadFile_SendKeys_1FileAtATime() {
         driver.get("https://blueimp.github.io/jQuery-File-Upload/");
-
-
-
+        By addFilesButton = By.cssSelector(".fileinput-button input[type='file']");
+        driver.findElement(addFilesButton).sendKeys(folderPathName + rabbit);
+        driver.findElement(addFilesButton).sendKeys(folderPathName + tiger);
+        driver.findElement(addFilesButton).sendKeys(folderPathName + hippo);
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='" + rabbit + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='" + tiger + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='" + hippo + "']")).isDisplayed());
+        List<WebElement> allStartButtons = driver.findElements(By.cssSelector("tbody button.start"));
+        for (WebElement button : allStartButtons) {
+            button.click();
+            sleepInSecond(2);
+        }
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + rabbit + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + tiger + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + hippo + "']")).isDisplayed());
     }
 
     @Test
-    public void TC_02_() {
-
+    public void TC_02_UploadFile_Multiple_Sendkeys() {
+        driver.get("https://blueimp.github.io/jQuery-File-Upload/");
+        By addFilesButton = By.cssSelector(".fileinput-button input[type='file']");
+        driver.findElement(addFilesButton).sendKeys(folderPathName + rabbit + "\n" + folderPathName + tiger + "\n" + folderPathName + hippo);
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='" + rabbit + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='" + tiger + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//p[text()='" + hippo + "']")).isDisplayed());
+        List<WebElement> allStartButtons = driver.findElements(By.cssSelector("tbody button.start"));
+        for (WebElement button : allStartButtons) {
+            button.click();
+            sleepInSecond(2);
+        }
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + rabbit + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + tiger + "']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + hippo + "']")).isDisplayed());
     }
 
     @Test
@@ -43,7 +80,7 @@ public class Topic_16_UploadFile {
 
     @AfterClass
     public void afterClass() {
-        driver.quit();
+       // driver.quit();
     }
 
     public void sleepInSecond (long timeInSecond) {
