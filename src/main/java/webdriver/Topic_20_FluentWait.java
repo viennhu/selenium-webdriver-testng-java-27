e60bf2516a48bb7f346a727739bf7d92f15ad247
Vivian Le <vivian.le@avepoint.com>
2022-07-29 15:18:55 +0700

    add new Topics

---
+++ src/main/java/webdriver/Topic_20_FluentWait.java
@@ -0,0 +1,91 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+public class Topic_20_FluentWait {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+    FluentWait<WebElement> fluentElement;
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("MAC OS")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        }
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Practice7() {
+        driver.get("https://automationfc.github.io/fluent-wait/");
+        WebElement time = driver.findElement(By.id("javascript_countdown_time"));
+        fluentElement = new FluentWait<WebElement>(time);
+        fluentElement.withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofMillis(100))
+                .ignoring(NoSuchElementException.class)
+                .until(new Function<WebElement, Boolean>() {
+                    @Override
+                    public Boolean apply(WebElement time) {
+                        boolean flag = time.getText().endsWith("00");
+                        return flag;
+                    }
+                });
+    }
+
+    @Test
+    public void TC_02_Practice8() {
+        driver.get("https://automationfc.github.io/dynamic-loading/");
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+        WebElement text = driver.findElement(By.xpath("//h4[text()='Hello World!']"));
+        FluentWait<WebElement> fluentElement;
+        fluentElement = new FluentWait<WebElement>(text);
+        fluentElement.withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofMillis(100))
+                .ignoring(NoSuchElementException.class)
+                .until(new Function<WebElement, Boolean>() {
+                    @Override
+                    public Boolean apply(WebElement text) {
+                        boolean textIsDisplayed = text.isDisplayed();
+                        return textIsDisplayed;
+                    }
+                });
+    }
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    public void sleepInSecond (long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int generateRandomNumber () {
+        Random rand = new Random();
+        return rand.nextInt(99999);
+    }
+}
